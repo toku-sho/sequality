d49d8759bfb5a23075bc8d923f7ec07c983e5423
unknown <sh.51to.21@gmail.com>
2024-07-18 13:53:09 +0900

    add gradle project

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -10,6 +10,7 @@ public class App {
 
     public static void main(String[] args) {
         System.out.println(new App().getGreeting());
+        Calculate calculate = new sum();
+        System.out.println(calculate);
     }
-    
 }
